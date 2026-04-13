0aecbb25d493e57c7fea89f05e0eed844b895a82
Chris Johnson <chris@users.noreply.github.com>
2026-04-13 22:09:41 +0700

    Bump version to 0.3.5

--- cmkview.py
+++ cmkview.py
@@ -20,7 +20,7 @@ import updater
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 POPUP_HTML_PATH = os.path.join(BASE_DIR, "popup.html")
 SETUP_HTML_PATH = os.path.join(BASE_DIR, "setup.html")
-__version__ = "0.3.0"
+__version__ = "0.3.5"
 
 SYSTEM_SOUNDS = [
     "Basso", "Blow", "Bottle", "Frog", "Funk", "Glass", "Hero",
